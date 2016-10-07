03da238fc9505217df231d4cdd1de6d6102df789
sarahdcard <sarah.d.card@gmail.com>
2016-10-07 19:50:13 -0400

    Submitting Jackson's Home Page Changes

--- index.android.js
+++ index.android.js
@@ -17,60 +17,110 @@ import {
 
 class WellCat extends Component {
 
-  constructor(props) {
-      super(props);
-      this.state = {text: ''};
-    }
-
-    render() {
-
-      return (
+  render() {
+    return (
       <View style={styles.container}>
-              <Text style={styles.welcome}>
-                Welcome to WellCat!
-              </Text>
-          <TextInput
-            style={{height: 40, width:150}}
-            placeholder="Username"
-            onChangeText={(text) => this.setState({text})}
-          />
-          <TextInput
-            style={{height: 40, width:150}}
-            placeholder="Password"
-            onChangeText={(text) => this.setState({text})}
-          />
-
-          <TouchableNativeFeedback
+        <View style={styles.layerOne}>
+        <Text style={styles.welcome}>
+          Welcome to WellCat!
+        </Text>
+        <TouchableNativeFeedback
               onPress={this._onPressButton}
               background={TouchableNativeFeedback.SelectableBackground()}>
-            <View style={{width: 150, height: 50, backgroundColor: 'grey'}}>
-              <Text style={{textAlign: 'center'}}>Submit</Text>
+            <View style={{width: 75, height: 25, backgroundColor: 'grey', margin: 2}}>
+              <Text style={{textAlign: 'center'}}>Login</Text>
             </View>
-          </TouchableNativeFeedback>
+        </TouchableNativeFeedback>
 
+        <TouchableNativeFeedback
+              onPress={this._onPressButton}
+              background={TouchableNativeFeedback.SelectableBackground()}>
+            <View style={{width: 75, height: 25, backgroundColor: 'grey', margin: 2}}>
+              <Text style={{textAlign: 'center'}}>Sign-Up</Text>
+            </View>
+        </TouchableNativeFeedback>
+      </View>
 
+      <Text style={styles.newsHeading}>Cat News</Text>
+
+      <View style={styles.messageBox}>
+        <View>
+          <Text style={styles.messageBoxTitleText}>Article 3 | Oct 9th, 2016</Text>
         </View>
-      );
-      }
+        <View>
+          <Text style={styles.messageBoxBodyText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel felis sed ipsum luctus congue eget quis augue.</Text>
+        </View>
+      </View>
+
+      <View style={styles.messageBox}>
+        <View>
+          <Text style={styles.messageBoxTitleText}>Article 2 | Oct 8th, 2016</Text>
+        </View>
+        <View>
+          <Text style={styles.messageBoxBodyText}>Sed tincidunt sodales mi ac pulvinar. Nunc ut pretium enim.</Text>
+        </View>
+      </View>
+
+      <View style={styles.messageBox}>
+        <View>
+          <Text style={styles.messageBoxTitleText}>Article 1 | Oct 7th, 2016</Text>
+        </View>
+        <View>
+          <Text style={styles.messageBoxBodyText}>Pellentesque sed convallis ante. Phasellus vitae tristique ligula. Nunc volutpat suscipit accumsan.</Text>
+        </View>
+      </View>
+
+
+    </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    flex: 0,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
     backgroundColor: '#F5FCFF',
+    paddingBottom:20,
+  },
+  layerOne: {
+    flex: 0,
+    flexDirection: 'row',
+    margin: 5,
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: 'left',
+    margin: 5,
   },
-  instructions: {
-    textAlign: 'center',
+  newsHeading: {
+    fontSize: 26,
+    textAlign: 'right',
     color: '#333333',
-    marginBottom: 5,
+    margin: 15,
+    marginBottom:10
   },
+  messageBox:{
+    backgroundColor:'grey',
+    paddingTop:10,
+    paddingBottom:20,
+  },
+  messageBoxTitleText:{
+    fontWeight:'bold',
+    color:'#fff',
+    textAlign:'left',
+    fontSize:18,
+    marginBottom:10,
+    margin: 15,
+  },
+  messageBoxBodyText:{
+    color:'#fff',
+    fontSize:16,
+    margin: 15,
+  }
+
 });
 
 AppRegistry.registerComponent('WellCat', () => WellCat);
