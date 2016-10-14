f53662bc7c9c19f004e95c4a7bc53b96a885e192
Rob Little <rob.little4412@gmail.com>
2016-10-14 13:49:02 -0400

    Refactored the main page into a component and added a very very basic signup component.

---
+++ components/signup.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text } from 'react-native';
+
+export default class SignUp extends Component {
+  render() {
+    return (
+      <Text>Hello world and SAM!</Text>
+    );
+  }
+}
+
